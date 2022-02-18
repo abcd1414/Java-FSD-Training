037dd407aa41b7bdced18628fc4460809e1e5f4a
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-18 02:20:05 -0500

    Add files via upload

---
+++ java-FSD-Training/java-programs/Variable.java
@@ -0,0 +1,16 @@
+class Variable
+{
+	public static void main(String[] args)
+		{
+			int a=20;
+			int b=30;
+			int sum=a+b;
+			int sub=a-b;
+			int mul=a*b;
+			int div=a/b;
+			System.out.println("sum=" +sum);
+			System.out.println("sub=" +sub);
+			System.out.println("mul=" +mul);
+			System.out.println("div=" +div);
+		}
+}
