2ab7c42f0cfcbe072a36d213c050112864f3596b
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-19 00:10:24 -0500

    Add files via upload

---
+++ java-FSD-Training/java-programs/LogicalOperator.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class LogicalOperator {
+public static void main(String[] args)
+{
+int a = 10;
+int b = 20;
+int c = 30;
+int d = 0;
+if ((a < b) && (c >b)) {
+System.out.println("True");
+}
+else{System.out.println("false");
+}
+if((a > b ) || (c==d));
+{
+System.out.println(" One condition isTrue");
+}
+else{System.out.println("false");
+}
+}
+}
