0ef099e645928bc90a04f00d0bc539d59eb3a658
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-19 00:08:58 -0500

    Add files via upload

---
+++ java-FSD-Training/java-programs/Abstract.java
@@ -0,0 +1,26 @@
+abstract class Courses {
+abstract void exam();
+abstract void treat();
+void study(){
+System.out.println("study study");
+}
+class Engg extends Courses{
+public void exam() {
+System.out.println("writing exam");
+}
+}
+class Doctor extends Courses
+{
+public void treat() {
+System.out.println("take treatement");
+}
+}
+class Abstract {
+public static void main(String[] args) {
+Engg e = new Engg();
+Doctor d= new Doctor();
+e.exam();
+d.treat();
+e.study();
+}
+}
