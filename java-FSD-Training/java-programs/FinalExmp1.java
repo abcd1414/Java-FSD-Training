02e5ea5d3999e11a88b4ce8778078574c15e37d0
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-20 07:55:22 -0500

    Add files via upload

---
+++ java-FSD-Training/java-programs/FinalExmp1.java
@@ -0,0 +1,14 @@
+class FinalExmp1
+{
+	final int a=5;
+	void test()
+	{
+		a=350;
+		System.out.println("a value after changing="+a);
+	}
+	public static void main(String[] args)
+	{
+		FinalExmp1 a=new FinalExmp1();
+		System.out.println("a ="+a);
+		a.test();
+	}
