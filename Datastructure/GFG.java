66d0d617aec5ead8d0f04d5506f943ab53e576d2
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-20 09:25:11 -0500

    Add files via upload

---
+++ Datastructure/GFG.java
@@ -0,0 +1,26 @@
+import java.util.*;
+  
+public class GFG {
+    public static void main(String[] args)
+    {
+        // Create an empty Hashtable
+        Hashtable<String, Integer> ht = new Hashtable<>();
+  
+        // Add elements to the hashtable
+        ht.put("vishal", 10);
+        ht.put("sachin", 30);
+        ht.put("vaibhav", 20);
+  
+        // Print size and content
+        System.out.println("Size of map is:- " + ht.size());
+        System.out.println(ht);
+  
+        // Check if a key is present and if
+        // present, print value
+        if (ht.containsKey("vishal")) {
+            Integer a = ht.get("vishal");
+            System.out.println("value for key"
+                               + " \"vishal\" is:- " + a);
+        }
+    }
+}
