788deb141c4fc3145d7dbc2e407be8e40b8ea5ed
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-20 03:47:43 -0500

    Add files via upload

---
+++ java-FSD-Training/java-programs/Exmp.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class Exmp
+{
+public static void main(String[] args)
+{
+System.out.println("Enter the size of array 1 ");
+Scanner sc = new Scanner(System.in);
+int a= sc.nextInt();
+int[] p = new int [a];
+System.out.println("Enter the elements of array 1 ");
+for(int i=0;i<a;i++)
+{
+p[i]=sc.nextInt();
+}
+System.out.println("The elements are ");
+for(int i=0;i<a;i++)
+{
+System.out.println(p[i]);
+}
+System.out.println("Enter the size of array 2 ");
+int b= sc.nextInt();
+int[] q=new int[b];
+System.out.println("enter the elements of array 2")
+for(int i=0;i<b;i++)
+{
+q[i]=sc.nextInt();
+}
+System.out.println("The elements are ");
+for(int i=0;i<b;i++)
+{
+System.out.println(q[i]);
+}
+}
+}
+
+{
+public static void main(String[] args)
+{
+System.out.println("Enter the size of array 1 ");
+Scanner sc = new Scanner(System.in);
+int a= sc.nextInt();
+int[] p = new int [a];
+System.out.println("Enter the elements of array 1 ");
+for(int i=0;i<a;i++)
+{
+p[i]=sc.nextInt();
+}
+System.out.println("The elements are ");
+for(int i=0;i<a;i++)
+{
+System.out.println(p[i]);
+}
+System.out.println("Enter the size of array 2 ");
+int b= sc.nextInt();
+int[] q=new int[b];
+System.out.println("enter the elements of array 2")
+for(int i=0;i<b;i++)
+{
+q[i]=sc.nextInt();
+}
+System.out.println("The elements are ");
+for(int i=0;i<b;i++)
+{
+System.out.println(q[i]);
+}
+}
+}
