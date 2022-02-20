b72b3082515d6d7485d23f59451cce9a4cc1e3b1
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-20 03:26:00 -0500

    Add files via upload

---
+++ java-FSD-Training/java-programs/Numb.java
@@ -0,0 +1,16 @@
+class Numb{
+public static void main(String [] rk){
+	   outer:	
+		for(int i=0; i<3; i++)   
+		  {
+		inner:
+		    for(int j=0; j<3; j++)
+			{
+			   System.out.println(i + ", "+ j);
+			   if(j==2) 	break inner;
+			   if(i==j)	continue outer; 
+                           System.out.println("Bye");	
+			}
+		  }
+	}
+        }
