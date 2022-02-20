d6402dbcd60e531cb849aa53aff864bd398ef8cf
Pooja R <47709215+abcd1414@users.noreply.github.com>
2022-02-20 08:32:38 -0500

    Add files via upload

--- java-FSD-Training/java-programs/GFG.java
+++ java-FSD-Training/java-programs/GFG.java
@@ -1,68 +1,26 @@
 import java.util.*;
-import java.io.*;
- 
-class GFG {
-    public static void main (String[] args) {
-         BST tree=new BST();
-        tree.insert(30);
-        tree.insert(50);
-        tree.insert(15);
-        tree.insert(20);
-        tree.insert(10);
-        tree.insert(40);
-        tree.insert(60);
-        tree.inorder();
-    }
-}
- 
-class Node{
-    Node left;
-    int val;
-    Node right;
-    Node(int val){
-        this.val=val;
-    }
-}
- 
-class BST{
-  Node root;
-   
-  public void insert(int key){
-        Node node=new Node(key);
-        if(root==null) {
-            root = node;
-            return;
-        }
-        Node prev=null;
-        Node temp=root;
-        while (temp!=null){
-            if(temp.val>key){
-                prev=temp;
-                temp=temp.left;
-            }
-            else if (temp.val<key){
-                prev=temp;
-                temp=temp.right;
-            }
-        }
-        if(prev.val>key)
-            prev.left=node;
-        else prev.right=node;
-    }
-   
-   public void inorder(){
-        Node temp=root;
-        Stack<Node> stack=new Stack<>();
-        while (temp!=null||!stack.isEmpty()){
-            if(temp!=null){
-                stack.add(temp);
-                temp=temp.left;
-            }
-            else {
-                temp=stack.pop();
-                System.out.print(temp.val+" ");
-                temp=temp.right;
-            }
+  
+public class GFG {
+    public static void main(String[] args)
+    {
+        // Create an empty Hashtable
+        Hashtable<String, Integer> ht = new Hashtable<>();
+  
+        // Add elements to the hashtable
+        ht.put("vishal", 10);
+        ht.put("sachin", 30);
+        ht.put("vaibhav", 20);
+  
+        // Print size and content
+        System.out.println("Size of map is:- " + ht.size());
+        System.out.println(ht);
+  
+        // Check if a key is present and if
+        // present, print value
+        if (ht.containsKey("vishal")) {
+            Integer a = ht.get("vishal");
+            System.out.println("value for key"
+                               + " \"vishal\" is:- " + a);
         }
     }
 }
